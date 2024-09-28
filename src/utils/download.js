a45665f8c474e84bad683e6c822e8351d71027f0
Bamiwo Adebayo <bammietop@gmail.com>
2024-09-29 00:12:53 +0100

    Portfolio

--- src/utils/download.js
+++ src/utils/download.js
@@ -1,6 +1,6 @@
 const handleDownload = () => {
   const link = document.createElement("a");
-  link.href = `./public/assets/cv.pdf`; // URL of the file
+  link.href = "/assets/cv.pdf"; // URL of the file
   link.download = "bamiwo_resume.pdf"; // Suggested file name for download
   link.click(); // Trigger the download
 };
